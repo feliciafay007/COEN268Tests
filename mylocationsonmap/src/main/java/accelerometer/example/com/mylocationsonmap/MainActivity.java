399781240476d6c88c298373bf099f7d910433fc
feliciafay007 <feliciafay@gmail.com>
2015-05-19 13:30:19 -0700

    Done: class example 2, show library, university and city on map.

--- mylocationsonmap/src/main/java/accelerometer/example/com/mylocationsonmap/MainActivity.java
+++ mylocationsonmap/src/main/java/accelerometer/example/com/mylocationsonmap/MainActivity.java
@@ -3,31 +3,49 @@ import android.os.Bundle;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
+import android.view.View;
 
 public class MainActivity extends Activity implements OnMapReadyCallback {
-
+    private  final LatLng LOCATION_UNIV = new LatLng(37.349642, -121.938987);
+    private  final LatLng LOCATION_BUILDING = new LatLng(37.348190, -121.937975);
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
+        map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+     //   map.addMarker(new MarkerOptions().position(LOCATION_BUILDING).title("Find Me Here!"));
+        onMapReady(map);
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
-
         map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LOCATION_BUILDING , 13));
         map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+                .title("Find Me Here!")
+                .snippet("Santa Clara University is located in the bay area.")
+                .position(LOCATION_BUILDING));
 
+    }
+
+    public void onClick_City(View v) {
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCATION_UNIV, 10);
+        map.animateCamera(update);
+    }
+
+    public void onClick_University(View v) {
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCATION_UNIV, 13);
+        map.animateCamera(update);
+    }
+
+    public void onClick_Building(View v) {
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCATION_BUILDING, 17);
+        map.animateCamera(update);
     }
 }
 
